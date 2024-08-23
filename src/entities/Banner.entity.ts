bbcfef6ce1094859020270e31c0c7e0dfb2e94d9
phucnh17 <phucnh17@msb.com.vn>
2024-08-23 15:38:43 +0700

    feat: banner

---
+++ src/entities/Banner.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { File } from "./File.entity.js";
+
+@Entity('banner')
+export class Banner extends File {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ nullable: true })
+  orderNo: number
+}
